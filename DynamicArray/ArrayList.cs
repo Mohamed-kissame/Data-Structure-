1cfb5d07b6f69602e39bbdc1e96ff4085a763f67
Mohamed <mkissame9@gmail.com>
2026-04-24 21:40:47 +0100

    feat(data-structures): implement custom dynamic array with resize and search

---
+++ DynamicArray/ArrayList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArray
+{
+    public class ArrayList
+    {
+
+        private int _Capacity;
+
+        private int _Count;
+
+        private int[] _storage;
+
+        public int GetCapacity { get => _Capacity; }
+
+        public int GetSize { get => _Count; }
+
+
+        public ArrayList()
+        {
+            _storage = new int[4];
+            _Capacity = _storage.Length;
+            _Count = 0;
+
+        }
+
+        private int[] CopyItems(int[]arr1)
+        {
+
+            for (int i = 0; i < _Count; i++)
+            {
+
+                arr1[i] = _storage[i];
+
+            }
+
+
+            return arr1;
+
+
+        }
+        
+
+        public void Add(int value)
+        {
+
+            if (_Count == _Capacity)
+            {
+
+                _Capacity *= 2;
+
+                int[] newArray = new int[_Capacity];
+
+                newArray = CopyItems(newArray);
+
+                _storage =  newArray;
+            }
+
+
+                _storage[_Count] = value;
+                _Count++;
+
+         
+        }
+
+        public void Display()
+        {
+
+            Console.Write("[");
+
+            for (int i = 0; i < _Count; i++)
+            {
+             
+                Console.Write($"{_storage[i]} , ");
+            
+                
+
+            }
+
+            Console.Write("]");
+
+        }
+
+
+        public int Search(int value)
+        {
+
+            for (int i = 0; i < _Count; i++)
+            {
+
+                if ( _storage[i] == value)
+                {
+
+                    return i;
+
+                }
+                
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ DynamicArray/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArray
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.Add(1);
+            arrayList.Add(2);
+            arrayList.Add(3);
+            arrayList.Add(4);
+            arrayList.Add(10);
+            arrayList.Display();
+
+            Console.WriteLine($"\nCapacity of this ArrayList its = {arrayList.GetCapacity}  ");
+
+            int Find = arrayList.Search(11);
+
+            if(Find != -1)
+            {
+
+                Console.WriteLine($"\nThe value of 10 Found at position :  {Find}");
+
+            }
+            else
+            {
+                Console.WriteLine("\nThe Number it dosent Found");
+            }
+
+
+                Console.ReadLine();
+
+
+        }
+    }
+}
