892ad10287dcc1ba973448820ff77530564e3b6c
Mohamed <mkissame9@gmail.com>
2026-04-26 00:20:21 +0100

    feat(ds): create custom matrix utility from scratch with core operations

---
+++ Matrix/MatrixUtility.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class MatrixUtility
+    {
+
+
+
+        private int[,] _matrix;
+        private int _rows;
+        private int _cols;
+
+
+        public MatrixUtility(int Row , int Col)
+        {
+
+            if (Row <= 0 || Col <= 0)
+                throw new ArgumentException("Rows and Columns must be greater than zero");
+
+            _rows = Row;
+            _cols = Col;
+
+            _matrix = new int[Row, Col];
+
+        }
+
+
+
+        public void SetValue(int row, int col , int value)
+        {
+
+            if(!((row >= 0 && row < _rows) && (col >= 0 && col < _cols))) return;
+
+           
+
+            _matrix[row, col] = value;
+
+        }
+
+        public int GetValue(int Row , int Col)
+        {
+            if (!((Row >= 0 && Row < _rows) && (Col >= 0 && Col < _cols))) return -1;
+
+            return _matrix[Row, Col];
+
+
+        }
+
+
+        public void Display()
+        {
+
+            for (int i = 0; i < _rows; i++)
+            {
+
+                Console.Write("[");
+
+                for (int j = 0; j < _cols; j++)
+                {
+
+                    Console.Write(_matrix[i,j]);
+
+                    if (j < _cols -1 )
+                    {
+                        Console.Write(",");
+                    }
+                    
+
+                }
+
+                Console.WriteLine("]");
+
+
+            }
+
+        }
+
+        public void FillSequential()
+        {
+
+            int counter = 1;
+
+
+            for (int i = 0; i < _rows; i++)
+            {
+
+                for (int j = 0; j < _cols; j++)
+                {
+
+                    _matrix[i, j] = counter++;
+
+                }
+
+            }
+
+        }
+
+        public int SumAll()
+        {
+
+            int sum = 0;
+
+            for (int i = 0; i < _rows; i++)
+            {
+
+                for (int j = 0; j < _cols; j++)
+                {
+
+                    sum += _matrix[i, j];
+
+                }
+
+            }
+
+            return sum;
+
+
+        }
+
+        public int RowSum(int Row)
+        {
+
+            if (Row < 0 || Row >= _rows)
+                return -1;
+
+            int sumRow = 0;
+
+
+            for (int i = 0; i < _cols; i++)
+            {
+                
+                sumRow += _matrix[Row,i];
+
+            }
+
+
+
+            return sumRow;
+
+
+        }
+
+
+        public int ColumnSum(int Col)
+        {
+
+            if (Col < 0 || Col >= _cols)
+                return -1;
+
+            int ColSum = 0;
+
+
+            for (int i = 0; i < _rows; i++)
+            {
+
+                ColSum += _matrix[i, Col];
+
+            }
+
+
+
+            return ColSum;
+
+
+        }
+
+
+
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MatrixUtility matrix = new MatrixUtility(3, 3);
+
+
+            matrix.SetValue(0, 0, 1);
+            matrix.SetValue(0, 1, 2);
+            matrix.SetValue(0, 2, 3);
+
+            matrix.SetValue(1, 0, 4);
+            matrix.SetValue(1, 1, 5);
+            matrix.SetValue(1, 2, 6);
+
+            matrix.SetValue(2, 0, 7);
+            matrix.SetValue(2, 1, 8);
+            matrix.SetValue(2, 2, 9);
+
+
+            matrix.Display();
+
+
+            //Console.WriteLine("\nEnter the Position of Row : ");
+            //int row = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.WriteLine("\nEnter the Position of Col : ");
+            //int col = Convert.ToInt32((Console.ReadLine()));
+
+
+            //Console.WriteLine($"\nthe value of position [{row}][{col}] equal : {matrix.GetValue(row , col)} ");
+
+            //Console.ReadLine();
+
+
+
+            //MatrixUtility matrix1 = new MatrixUtility(4, 4);
+
+            //matrix1.FillSequential();
+
+            //matrix1.Display();
+
+
+            //int sum = matrix.SumAll();
+
+            //Console.WriteLine($"The Total of all values inside the matrix its : {sum} ");
+
+
+            //Console.WriteLine("\nEnter the Position of Row that you wanna to sum : ");
+            //int row = Convert.ToInt32(Console.ReadLine());
+
+            //int sumRow = matrix.RowSum(row);
+
+            //Console.WriteLine($"\nThe Total of all values inside the Row {row} its : {sumRow} ");
+
+
+            Console.WriteLine("\nEnter the Position of Col that you wanna to sum : ");
+            int Col = Convert.ToInt32(Console.ReadLine());
+
+            int ColumnSum = matrix.ColumnSum(Col);
+
+            Console.WriteLine($"\nThe Total of all values inside the Column {Col} its : {ColumnSum} ");
+
+
+
+
+
+        }
+    }
+}
