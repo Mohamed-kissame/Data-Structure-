454c3357dcd95e518f2ff3f1050d9558dd3c9b1b
Mohamed <mkissame9@gmail.com>
2026-04-26 23:51:10 +0100

    feat(ds): implement custom generic stack with dynamic resizing

---
+++ StackImplement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackImplement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+
+
+            stack.Display();
+
+            stack.Pop();
+
+            Console.WriteLine("\nAfter Delete the Last item in : ");
+
+            stack.Display();
+
+            Console.WriteLine($"\nThe item that at the top is : {stack.Peek()}");
+
+
+        }
+    }
+}
---
+++ StackImplement/Stack.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackImplement
+{
+    public class Stack<T>
+    {
+
+        private T[] _Storage;
+        private int _Count;
+        private int _Capacity;
+
+
+        public int Capacity => _Capacity;
+
+        public int Count => _Count;
+
+
+        public Stack()
+        {
+            _Capacity = 4;
+            _Storage = new T[_Capacity];
+            _Count = 0;
+
+        }
+
+        private T[] CopyItems(T[] arr)
+        {
+
+            for (int i = 0; i < _Count; i++)
+            {
+                arr[i] = _Storage[i];
+            }
+
+            return arr;
+        }
+
+
+        private void Resize()
+        {
+
+            _Capacity *= 2;
+
+            T[] arr = new T[_Capacity];
+
+             CopyItems(arr);
+
+            _Storage = arr;
+
+        }
+
+
+        public void Push(T value)
+        {
+
+            if(_Count == _Capacity)
+            {
+
+                Resize();
+
+            }
+
+            _Storage[_Count] = value;
+            _Count++;
+
+
+        }
+
+
+        public T Pop()
+        {
+
+            if(IsEmpty()) { return default(T); }
+
+            T value = _Storage[_Count - 1];
+            _Storage[_Count - 1] = default;
+            _Count--;
+
+
+            return value;
+
+        }
+
+        public T Peek()
+        {
+
+            if (IsEmpty()) { return default(T); }
+
+
+            return _Storage[_Count - 1];
+
+        }
+
+        public bool IsEmpty()
+        {
+            return (_Count == 0);
+        }
+
+
+        public void Display()
+        {
+
+            Console.Write("[");
+
+            for (int i = 0; i < _Count; i++)
+            {
+                Console.Write($"{_Storage[i]}");
+
+                if (i < _Count -1 )
+                {
+                   
+                    Console.Write(" , ");
+
+                }
+
+            }
+
+            Console.Write("]");
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
