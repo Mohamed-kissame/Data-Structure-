3ddfcd69deebb4629db36912e2b072f0c60a2528
Mohamed <mkissame9@gmail.com>
2026-04-28 00:14:56 +0100

    feat(ds): implement custom generic queue with simple array shifting

---
+++ QueueImplement/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueImplement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+
+            queue.Display();
+
+            Console.WriteLine($"\nThe Font of this Queue its  {queue.Peek()}");
+
+            Console.WriteLine("\nAfter Dequeue Queue ");
+
+            queue.Dequeue();
+
+            queue.Display();
+
+            Console.WriteLine("\nAfter Dequeue Queue  ");
+
+            queue.Dequeue();
+            queue.Display();
+
+
+
+        }
+    }
+}
