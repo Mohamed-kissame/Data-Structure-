d696918353407a4f586d9bda4127453c1a645fe6
Mohamed <mkissame9@gmail.com>
2026-05-04 19:05:13 +0100

    feat(project): add call center waiting buffer using circular queue

---
+++ Mini-Projects/Call Center Waiting Buffer/CallCenterWaitingBuffer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Call_Center_Waiting_Buffer
+{
+    public class CallCenterWaitingBuffer
+    {
+
+        private CircularQueue<string> _Callers;
+
+        public CallCenterWaitingBuffer()
+        {
+            _Callers = new CircularQueue<string>();
+        }
+
+        public void AddCaller(string callerName)
+        {
+
+            if (string.IsNullOrWhiteSpace(callerName))
+            {
+
+                Console.WriteLine("Should Enter an Valid Name");
+                return;
+            }
+
+            _Callers.Enqueue(callerName);
+
+        }
+
+        public void ServeNextCaller()
+        {
+            if (_Callers.IsEmpty()) { Console.Write("No callers waiting"); return; }
+
+            string caller = _Callers.Dequeue();
+
+            Console.WriteLine($"Serving : {caller}");
+        }
+
+        public void PeekNextCaller()
+        {
+
+            if (_Callers.IsEmpty()) { Console.WriteLine("No next Caller "); return; }
+
+            string next = _Callers.Peek();
+
+            Console.WriteLine($"Next caller : {next}");
+
+        }
+
+        public void ShowWaitingCallers()
+        {
+
+            if (_Callers.IsEmpty()) { Console.Write("No callers waiting"); return; }
+
+            Console.Write("Waiting callers : ");
+
+            _Callers.Display();
+
+            Console.WriteLine();
+
+        }
+
+        public bool HasCallers() => !_Callers.IsEmpty();
+    }
+}
---
+++ Mini-Projects/Call Center Waiting Buffer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Call_Center_Waiting_Buffer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CallCenterWaitingBuffer callCenterWaitingBuffer = new CallCenterWaitingBuffer();
+
+            callCenterWaitingBuffer.AddCaller("Mohamed");
+            callCenterWaitingBuffer.AddCaller("Youssef");
+            callCenterWaitingBuffer.AddCaller("Amin");
+
+            callCenterWaitingBuffer.PeekNextCaller();
+            callCenterWaitingBuffer.ServeNextCaller();
+            callCenterWaitingBuffer.ServeNextCaller();
+            callCenterWaitingBuffer.AddCaller("Adam");
+            callCenterWaitingBuffer.ShowWaitingCallers();
+
+
+        }
+    }
+}
